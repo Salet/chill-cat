4031f2e5de31762070a5d8a5684fcd3da2f71519
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2020-11-16 10:56:49 +0100

    changes

---
+++ pages/gallery.vue
@@ -0,0 +1,3 @@
+<template>
+  <span>This is gallery view</span>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,5 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">chill-cat</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div class="w-full text-left">This is a website about Chill Cat</div>
 </template>
 
 <script>
