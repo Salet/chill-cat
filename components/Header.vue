a567c43ad29454d6c902b7ea9c44316be65265c6
Mateusz Chrapczynski <mateusz.chrapczynski@pubnub.com>
2020-11-16 11:37:08 +0100

    anim beh

--- components/Header.vue
+++ components/Header.vue
@@ -3,6 +3,7 @@
     <div class="flex items-center">
       <NuxtLink to="/"><h1 class="text-2xl">Chill Cat</h1></NuxtLink>
       <NuxtLink to="/gallery" class="ml-8">Gallery</NuxtLink>
+      <NuxtLink to="/animal-behavior" class="ml-8">Animal behavior</NuxtLink>
       <!-- <NuxtLink to="/anatomy" class="ml-8">Anatomy</NuxtLink> -->
     </div>
     <img src="~/assets/cat.png" class="w-64 rounded-xl" />
---
+++ pages/animal-behavior.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="max-w-sm w-full lg:max-w-full lg:flex">
+      <div
+        class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+      ></div>
+      <div
+        class="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+      >
+        <div class="mb-8">
+          <div class="text-gray-900 font-bold text-xl mb-2">
+            How cat gets his food?
+          </div>
+          <p class="text-gray-700 text-base">
+            Despite being specifically adapted to hunt for food, domestic cats
+            will usually take advantage of alternative food sources that may be
+            available, such as food found through scavenging or in the case of
+            pet cats, food that is provided for them by their owners.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
