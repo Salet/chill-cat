666d3133e7be2dac8345aed0cb7538e140fc4de3
Mateusz Chrapczynski <mateusz.chrapczynski@pubnub.com>
2020-11-16 12:02:21 +0100

    ico

--- components/Header.vue
+++ components/Header.vue
@@ -2,20 +2,10 @@
   <div>
     <div class="mb-16">
       <nav
-        class="w-full flex items-center justify-between flex-wrap bg-teal-500 p-6"
+        class="w-full flex items-center justify-between flex-wrap bg-black p-6"
       >
         <div class="flex items-center flex-shrink-0 text-white mr-6">
-          <svg
-            class="fill-current h-8 w-8 mr-2"
-            width="54"
-            height="54"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
-            />
-          </svg>
+          <img src="~/assets/cat-ico.png" />
           <NuxtLink to="/">
             <span class="font-semibold text-xl tracking-tight">
               Chill Cat
@@ -77,6 +67,13 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.ico {
+  width: 50px;
+}
+.bg-black {
+  background-color: black;
+}
+</style>
 
 <style scoped></style>
