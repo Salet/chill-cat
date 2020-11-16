4ef35add9ec25707bd309960f2ecf218e137297d
Przemysław Janowski <przemyslaw.janowski@pubnub.com>
2020-11-16 11:04:49 +0100

    gallery

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -1,3 +1,16 @@
 <template>
-  <span>This is gallery view</span>
+  <div class="flex">
+    <img src="~/assets/1.jpg" alt="" />
+    <img src="~/assets/2.jpg" alt="" />
+    <img src="~/assets/3.jpg" alt="" />
+  </div>
 </template>
+
+<style scoped>
+img {
+  @apply rounded-xl;
+  width: 300px;
+  height: 250px;
+  margin-left: 20px;
+}
+</style>
