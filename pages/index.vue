fce222f1bab1330b234f737c5722fbff9381b72d
Mateusz Chrapczynski <mateusz.chrapczynski@pubnub.com>
2020-11-17 12:05:54 +0100

    last toych

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,17 @@
       <img class="w-full" src="~/assets/cat.png" alt="Sunset in the mountains" />
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">Hello Cat Lovers!</div>
-        <p class="text-gray-700 text-base">
+        <p class="text-gray-700 text-base mb-2">
           This is official page of the chill cat, the domestic cat from Katowice. We'd like to
           present you details and animal habitat, anatomy, behaviour, diet and other interesting
-          facts. Our Mission: We present the chill cat for primary school children, who need to
-          prepare for school projects. The website is a collection of great resources that helps
-          kids create great presentations and essays. Unlike Wikipedia & other online resources, Our
-          Product is targeted specifically for children aged 6-10.
+          facts.
+        </p>
+        <p>Our Mission:</p>
+        <p class="text-gray-700 text-base">
+          We present the chill cat for primary school children, who need to prepare for school
+          projects. The website is a collection of great resources that helps kids create great
+          presentations and essays. Unlike Wikipedia & other online resources, Our Product is
+          targeted specifically for children aged 6-10.
         </p>
       </div>
     </div>
